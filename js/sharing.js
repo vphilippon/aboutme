832470ece36b9f28bdb0060823cc05857ff8596f
Vincent Philippon <sindaewoh@gmail.com>
2015-02-15 21:10:38 -0500

    oops

--- js/sharing.js
+++ js/sharing.js
@@ -1,5 +1,5 @@
 // Facebook SDK initialisation
-function(d, s, id) {
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
